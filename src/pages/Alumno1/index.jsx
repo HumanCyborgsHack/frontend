f406c1a2a8f60a0729df39a32fea85b008d5cdd9
HectorMtz22 <hec.gre81@gmail.com>
2022-07-23 01:15:55 -0500

    Add feature isBack on header and remove unnecesary titles

--- src/pages/Alumno1/index.jsx
+++ src/pages/Alumno1/index.jsx
@@ -5,27 +5,29 @@ const props = {
     name: "Pedro"
 };
 
+const path = "/alumno-1";
+
 const data = [
     {
         id: 1,
         subject: "Matemáticas",
         in: "30 min",
         color: "#66d399",
-        link: "/matematicas",
+        link: path + "/matematicas",
         img: "https://thecatapi.com/api/images/get?format=src&type=gif"
     },
     {
         id: 2,
         subject: "Habla Inglesa",
         color: "#d39966",
-        link: "/ingles",
+        link: path + "/ingles",
         img: "https://thecatapi.com/api/images/get?format=src&type=image"
     },
     {
         id: 3,
         subject: "Ciencias Naturales",
         color: "#a876ef",
-        link: "/naturales",
+        link: path + "/naturales",
         img: "https://thecatapi.com/api/images/get?format=src&type=image"
     }
 ];
---
+++ src/pages/Clase/index.jsx
@@ -0,0 +1,8 @@
+import Header from "@/components/Header";
+export default function Clase() {
+    return (
+        <>
+            <Header isBack searchDisabled></Header>
+        </>
+    );
+}
