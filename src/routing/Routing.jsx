99ed76282efbacdf418d6b453c908a4eb0c19b09
Sebastian Marines <sebastian0marines@gmail.com>
2022-07-23 03:54:39 -0500

    Create register page

--- src/routing/Routing.jsx
+++ src/routing/Routing.jsx
@@ -3,6 +3,7 @@ import { Default } from "@/pages/Default";
 
 import Alumno1 from "../pages/Alumno1";
 import Clase from "../pages/Clase";
+import Login from "../pages/Login";
 //import Alumno2 from "@/pages/Alumno2/Alumno2";
 import { useGlobalContext } from "@/context/global";
 
@@ -15,6 +16,8 @@ export function Routing() {
                 <Route path="/" element={<Default />}>
                     <Route path="*" element={<Default />} />
                 </Route>
+                <Route path="/register" element={<Login />} />
+
                 {user.role === "alumno" && (
                     <Route path="alumno-1">
                         <Route index element={<Alumno1 />} />
